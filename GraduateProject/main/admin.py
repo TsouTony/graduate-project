4ea4d4ae65e9ceaaf38ff8a708e2adeb30407b08
TsouTony <51122236+TsouTony@users.noreply.github.com>
2019-10-14 23:45:06 +0800

    Add files via upload

--- GraduateProject/main/admin.py
+++ GraduateProject/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Img, User, Commend
 # Register your models here.
+class adminIMG(admin.ModelAdmin):
+    list_display = ('img_url','create_time','computerScore')
+admin.site.register(Img,adminIMG)
+
+admin.site.register(User)
+
+admin.site.register(Commend)
---
+++ GraduateProject/main/setPath.py
@@ -0,0 +1,2 @@
+imgDirectoryPath = 'C:/Users/user/Desktop/Tony/GraduateProject/media'
+h5WeightPath ='C:/Users/user/Desktop/Tony/GraduateProject/main/weights/mobilenet_weights.h5'
